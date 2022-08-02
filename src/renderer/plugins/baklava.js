7480b441a40d046ca6baa1788f9274a0d2094a84
BiggiePete <pter.cross222@gmail.com>
2022-08-02 02:53:52 -0400

    updates gitignore to keep edit to baklavajs

--- src/renderer/plugins/baklava.js
+++ src/renderer/plugins/baklava.js
@@ -3,4 +3,5 @@ import {
   BaklavaVuePlugin
 } from "@baklavajs/plugin-renderer-vue";
 import "@baklavajs/plugin-renderer-vue/dist/styles.css";
+import Node from "@baklavajs/core/dist/baklavajs-core/src/node.d.ts"
 Vue.use(BaklavaVuePlugin);
