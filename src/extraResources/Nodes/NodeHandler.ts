d94bd3068473e99e9eb97402ddc2c9e815ca3210
BiggiePete <pter.cross222@gmail.com>
2022-08-03 19:01:47 -0400

    continues python parsing

--- src/extraResources/Nodes/NodeHandler.ts
+++ src/extraResources/Nodes/NodeHandler.ts
@@ -4,10 +4,23 @@ import CustomNodes from "./CustomNodes.json"
 export function NodeHandler(url) {
     const fs = require('fs')
     const file: string = fs.readFileSync(url, { encoding: 'utf8', flag: 'r' })
+
     const publisherRegExp = /rospy.Publisher/gim
     const subscriberRegExp = /rospy.Subscriber/gim
-    Array.from(file.matchAll(publisherRegExp))
-    Array.from(file.matchAll(subscriberRegExp))
+
+    const Publishers = Array.from(file.matchAll(publisherRegExp))
+    const Subscribers = Array.from(file.matchAll(subscriberRegExp))
+
+    extractParams(false, Publishers[0].index!, file)
+}
+
+function extractParams(isSubscriber: boolean, index: number, file: string) {
+    var params = {
+        isSubscriber: new Boolean(),
+        dataType: new String(),
+    };
+    params.isSubscriber = isSubscriber;
+    params.dataType = file.slice(file.indexOf(",", index) + 2, file.indexOf(",", file.indexOf(",", index) + 1))
 }
 
 function AppendtoCustomNodes() {
