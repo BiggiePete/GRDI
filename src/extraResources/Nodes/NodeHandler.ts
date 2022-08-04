bb366b398461062f109162f7b9c75ae4f134d0fd
BiggiePete <pter.cross222@gmail.com>
2022-08-03 21:50:14 -0400

    Adds importing python files for custom nodes

--- src/extraResources/Nodes/NodeHandler.ts
+++ src/extraResources/Nodes/NodeHandler.ts
@@ -1,7 +1,6 @@
 
-import CustomNodes from "./CustomNodes.json"
 
-export function NodeHandler(url) {
+export function NodeHandler(url: string) {
     const fs = require('fs')
     const file: string = fs.readFileSync(url, { encoding: 'utf8', flag: 'r' })
 
@@ -11,18 +10,51 @@ export function NodeHandler(url) {
     const Publishers = Array.from(file.matchAll(publisherRegExp))
     const Subscribers = Array.from(file.matchAll(subscriberRegExp))
 
-    extractParams(false, Publishers[0].index!, file)
+    if (url.includes("\\")) {
+        var fileName = url.slice(url.lastIndexOf("\\") + 1, url.lastIndexOf("."))
+    } else {
+        var fileName = url.slice(url.lastIndexOf("/") + 1, url.lastIndexOf("."))
+    }
+
+    //create the node datatype : 
+    var nodeParams = {
+        name: "",
+        inputs: [{}],
+        outputs: [{}],
+    }
+    nodeParams.name = fileName
+    Publishers.forEach(pub => {
+        nodeParams.outputs.push(extractParams(false, pub.index, file))
+    });
+    Subscribers.forEach(sub => {
+        nodeParams.inputs.push(extractParams(true, sub.index, file))
+    });
+    AppendtoCustomNodes(nodeParams)
 }
 
-function extractParams(isSubscriber: boolean, index: number, file: string) {
+function extractParams(isSubscriber: boolean, index: number | undefined, file: string) {
+    if (index == undefined) {
+        return {};
+    }
     var params = {
-        isSubscriber: new Boolean(),
-        dataType: new String(),
+        isInput: isSubscriber,
+        dataType: "",
     };
-    params.isSubscriber = isSubscriber;
+    params.isInput = isSubscriber;
     params.dataType = file.slice(file.indexOf(",", index) + 2, file.indexOf(",", file.indexOf(",", index) + 1))
+
+    return params
 }
 
-function AppendtoCustomNodes() {
-
+function AppendtoCustomNodes(Node) {
+    const fs = require('fs')
+    try {
+        var CustomNodes = fs.readFileSync("./resources/CustomNodes.json")
+        CustomNodes = JSON.parse(CustomNodes)
+    } catch (error) {
+        console.warn(error)
+    }
+    CustomNodes.push(Node);
+    console.log(CustomNodes)
+    fs.writeFile("./resources/CustomNodes.json", JSON.stringify(CustomNodes), (err) => { console.warn(err); console.log("file written") })
 }
