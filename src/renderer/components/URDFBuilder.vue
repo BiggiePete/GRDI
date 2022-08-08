50d4d02c82fd3d4338c8209bb9d289b49a7c4a55
BiggiePete <pter.cross222@gmail.com>
2022-08-08 18:51:23 -0400

    adds example to design tab

--- src/renderer/components/URDFBuilder.vue
+++ src/renderer/components/URDFBuilder.vue
@@ -1,55 +1,80 @@
-<template>
-    <div>
-        <div class="builder">
-
-        </div>
-    </div>
+<template lang="pug">
+div(ref='canvas')
 </template>
 
-<script lang="ts">
-import * as THREE from "three"
-//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+<script>
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 export default {
-    mounted() {
+    name: "URDFBuilder",
+    data: function () {
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const clock = new THREE.Clock();
-        const renderer = new THREE.WebGLRenderer({
-            powerPreference: "high-performance"
-        });
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.getElementsByClassName("builder")[0].appendChild(renderer.domElement);
-
-        const geometry = new THREE.BoxGeometry();
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        )
+        const renderer = new THREE.WebGLRenderer({ antialias: true })
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
         const material = new THREE.MeshBasicMaterial({
             color: 0xa7cca7,
-            wireframe: true,
-        });
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-
-        //const controls = new OrbitControls(camera, renderer.domElement);
-        //controls.update();
-
-        camera.position.z = 5;
-
-        function animate() {
-            requestAnimationFrame(animate);
-            const delta = clock.getDelta();
-            //controls.update()
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-
-            renderer.render(scene, camera);
-        };
-
-        animate();
-
+            wireframe: true
+        })
+        const cube = new THREE.Mesh(geometry, material)
+        return {
+            scene: scene,
+            camera: camera,
+            controls: [],
+            renderer: renderer,
+            cube: cube,
+            speed: 0.01
+        }
+    },
+    created: function () {
+        this.scene.add(this.camera)
+        this.scene.add(this.cube)
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.camera.position.z = 5
+        this.scene.background = new THREE.Color('rgb(17,17,17)')
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+        window.addEventListener('resize', this.onWindowResize, false)
+    },
+    mounted: function () {
+        this.$refs.canvas.appendChild(this.renderer.domElement)
+        this.animate()
+    },
+    methods: {
+        animate: function () {
+            requestAnimationFrame(this.animate)
+            this.renderer.render(this.scene, this.camera)
+            //this.cube.rotation.y += this.speed
+            //this.controls.update()
+        },
+        onWindowResize() {
+            this.camera.aspect = window.innerWidth / window.innerHeight
+            this.camera.updateProjectionMatrix()
+            this.renderer.setSize(window.innerWidth, window.innerHeight)
+            this.animate()
+        }
+    },
+    computed: {
+        rotate: function () {
+            if (this.speed === '') {
+                return 0
+            } else {
+                return this.speed
+            }
+        }
     }
 }
 </script>
 
 <style>
+#URDFBuilder_Container {
+    width: 100%;
+    height: 100%;
+}
 </style>
 
 <!--
